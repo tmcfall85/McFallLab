60c77f539e4ad006aa42c7e910f6d2c5d1d8cc69
Matthew Sochor <matthew.sochor@gmail.com>
2025-03-26 10:59:06 -0500

    Change suffix to just .gz

--- mcw_utils/rna_seq_pipeline/move.py
+++ mcw_utils/rna_seq_pipeline/move.py
@@ -19,7 +19,7 @@ def move_rcc_file_to_scratch(
             rna_folder_exists = True
             for item in acc_rna_folder.iterdir():
                 print(item)
-                if item.suffix == ".tar.gz" and item.is_file():
+                if item.suffix == ".gz" and item.is_file():
                     print("hiya!!")
                     fasta_file_exists = True
                     dest_folder = scratch_dir / item.stem
