f12f4f5221449df2d7ca2873b156e04cca89ef60
Matthew Sochor <matthew.sochor@gmail.com>
2025-03-26 10:20:29 -0500

    Create move file to copy RNA fastas to scratch

---
+++ mcw_utils/rna_seq_pipeline/move.py
@@ -0,0 +1,77 @@
+import shutil
+from pathlib import Path, PosixPath
+
+import pandas as pd
+import sys
+
+
+def move_rcc_file_to_scratch(
+    accession_number: str, group_dir: PosixPath, scratch_dir: PosixPath
+):
+
+    acc_folder = group_dir / accession_number
+    if acc_folder.is_dir():
+        acc_number_exists = True
+        acc_rna_folder = acc_folder / "RNA"
+        if acc_rna_folder.is_dir():
+            rna_folder_exists = True
+            for item in acc_rna_folder.iterdir():
+                if item.suffix == ".fasta.gz" and item.is_file():
+                    fasta_file_exists = True
+                    dest_file = scratch_dir / item.name
+                    shutil.copy(str(item), str(dest_file))
+            else:
+                fasta_file_exists = False
+        else:
+            rna_folder_exists = False
+            fasta_file_exists = False
+    else:
+        acc_number_exists = False
+        rna_folder_exists = False
+        fasta_file_exists = False
+
+    return acc_number_exists, rna_folder_exists, fasta_file_exists
+
+
+def move_rcc_files_to_scratch(
+    accession_numbers: list[str],
+    group_dir: PosixPath,
+    scratch_dir: PosixPath,
+):
+    acc_numbers_exists = []
+    rna_folders_exists = []
+    fasta_files_exists = []
+    tempus_dirs = []
+    for accession_number in accession_numbers:
+        for tempus_dir in group_dir.iterdir():
+            if tempus_dir.is_dir():
+                acc_number_exists, rna_folder_exists, fasta_file_exists = (
+                    move_rcc_file_to_scratch(accession_number, tempus_dir, scratch_dir)
+                )
+                acc_numbers_exists.append(acc_number_exists)
+                rna_folders_exists.append(rna_folder_exists)
+                fasta_files_exists.append(fasta_file_exists)
+                tempus_dirs.append(tempus_dir / accession_number)
+
+    move_report = pd.DataFrame(
+        {
+            "tempus_dir": tempus_dirs,
+            "acc_number_exists": acc_numbers_exists,
+            "rna_folder_exists": rna_folders_exists,
+            "fasta_file_exists": fasta_files_exists,
+        }
+    )
+    move_report.to_csv(scratch_dir / "move_report.csv", index=False)
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    in_dir = sys.argv2[2]
+    out_dir = sys.argv[3]
+    acc_file = pd.read_csv(sys.argv[4])
+
+    move_rcc_files_to_scratch(
+        acc_file.accession_id.values,
+        Path(f"/group/{user}/{in_dir}"),
+        Path(f"/scratch/g/{user}/{out_dir}/"),
+    )
