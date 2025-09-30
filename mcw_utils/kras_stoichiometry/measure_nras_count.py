b4a99baea57af1621bf3f3c34c159a729b40dd3a
Matthew Sochor <matthew.sochor@gmail.com>
2025-09-30 14:39:49 -0500

    iterate over directory and write to combined files

--- mcw_utils/kras_stoichiometry/measure_nras_count.py
+++ mcw_utils/kras_stoichiometry/measure_nras_count.py
@@ -7,30 +7,49 @@ import argparse
 def main(target_dir, fname_seq, out_dir):
     target_dir = Path(target_dir)
     isoform_list = ["ENST00000256078.8"]
+    left_count = {}
+    right_count = {}
+    left_edge = {}
+    right_edge = {}
     for item in target_dir.iterdir():
         if item.is_dir():
+            acc_num = item.stem
             print(f"Found folder: {item}")
             fname_sorted = (
                 item / "star_output" / "Aligned.toTranscriptome.sorted.out.bam"
             )
-            isoforms = Parsef(
-                bam_fname=fname_sorted, seq_fname=fname_seq, isoform_list=isoform_list
-            )
+            try:
+                isoforms = Parsef(
+                    bam_fname=fname_sorted,
+                    seq_fname=fname_seq,
+                    isoform_list=isoform_list,
+                )
 
-            print(f"Creating output directory: {out_dir}")
-            out_dir = item / out_dir
-            out_dir.mkdir(parents=True, exist_ok=True)
-            print("Measuring distributions")
-            isoforms.measure_distributions()
-            left_hist_dist = isoforms.left_start_distributions[isoform_list[0]]
-            df = pd.DataFrame(
-                {"count": list(left_hist_dist[0]) + [0], "edge": left_hist_dist[1]}
-            ).to_csv(out_dir / f"kras_left_start_distribution.csv", index=False)
+                print(f"Creating output directory: {out_dir}")
+                print("Measuring distributions")
+                isoforms.measure_distributions()
+                left_hist_dist = isoforms.left_start_distributions[isoform_list[0]]
+                right_hist_dist = isoforms.right_start_distributions[isoform_list[0]]
+                left_count[acc_num] = list(left_hist_dist[0])
+                right_count[acc_num] = list(right_hist_dist[0])
+                left_edge[acc_num] = list(left_hist_dist[1])
+                right_edge[acc_num] = list(right_hist_dist[1])
+            except Exception as e:
+                print(f"Error processing {acc_num}: {e}")
+                continue
 
-            right_hist_dist = isoforms.right_start_distributions[isoform_list[0]]
-            df = pd.DataFrame(
-                {"count": list(right_hist_dist[0]) + [0], "edge": right_hist_dist[1]}
-            ).to_csv(out_dir / f"kras_right_start_distribution.csv", index=False)
+    out_dir = Path(out_dir)
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    pd.DataFrame(left_count).to_csv(
+        out_dir / f"kras_left_start_distribution.csv", index=False
+    )
+
+    pd.DataFrame(right_count).to_csv(
+        out_dir / f"kras_right_start_distribution.csv", index=False
+    )
+    pd.DataFrame(left_edge).to_csv(out_dir / f"kras_left_start_edge.csv", index=False)
+    pd.DataFrame(right_edge).to_csv(out_dir / f"kras_right_start_edge.csv", index=False)
 
 
 if __name__ == "__main__":
