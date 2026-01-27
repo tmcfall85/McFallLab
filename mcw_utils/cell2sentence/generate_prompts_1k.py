a3298e62d3d82447dc16e6eccd756cf3dbad68d5
Matthew Sochor <matthew.sochor@gmail.com>
2026-01-27 12:42:00 -0600

    tweak generate prompts

--- mcw_utils/cell2sentence/generate_prompts_1k.py
+++ mcw_utils/cell2sentence/generate_prompts_1k.py
@@ -11,10 +11,9 @@ early_mid_late_panc_data = early_mid_late_data[early_mid_late_data.is_panc == Tr
 print("early_mid_late_panc_data shape:", early_mid_late_panc_data.shape)
 prompts = []
 acc_ids = []
-df_sig_1k = model_data[model_data.top_1000_sig == True].copy()
-df_sig_1k = df_sig_1k[df_sig_1k.gene_name.notnull()].copy()
-df_sig_1k.set_index("gene_name", inplace=True)
-big_data_genes = df_sig_1k.drop(columns=["top_1000_sig", "top_500_sig"])
+model_data = model_data[model_data.gene_name.notnull()].copy()
+model_data.set_index("gene_name", inplace=True)
+big_data_genes = model_data.drop(columns=["top_1000_sig", "top_500_sig"])
 gene_count = 0
 for c in big_data_genes.columns:
     if c in early_mid_late_panc_data.accession_id.values:
@@ -22,7 +21,7 @@ for c in big_data_genes.columns:
         df_acc_id = big_data_genes[c].copy()
         df_acc_id.sort_values(ascending=False, inplace=True)
 
-        gene_count = len(big_data_genes)
+        gene_count = 10
 
         cell_sentence = " ".join(df_acc_id[:gene_count].index)
         # cell_sentence = "MALAT1 TMSB4X B2M EEF1A1 H3F3B ACTB FTL RPL13 ..." # Truncated for example, use at least 200 genes for inference
@@ -36,7 +35,7 @@ for c in big_data_genes.columns:
         prompts.append(prompt)
         acc_ids.append(c)
 prompt_df = pd.DataFrame({"accession_id": acc_ids, "prompt": prompts})
-out_fname = f"prompts_{gene_count}_deg_{str(datetime.now()).replace(' ','_')}"
+out_fname = f"prompts_{gene_count}_highest_tpm_{str(datetime.now()).replace(' ','_')}"
 prompt_df.to_csv(
     f"{out_fname}.csv",
     index=False,
